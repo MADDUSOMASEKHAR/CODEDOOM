466638c852ea6be0c8ba11d1bd14ef53ced3c1c9
MADDUSOMASEKHAR <53407284+MADDUSOMASEKHAR@users.noreply.github.com>
2019-08-09 19:48:04 +0530

    Update SumofNumbers.java

--- SumofNumbers.java
+++ SumofNumbers.java
@@ -1,40 +1,27 @@
-package world;
+package summation;
 
-import java.util.Scanner;
 
 public class SumofNumbers {
-	public int SumofEven(int a,int b)
+	
+	public int SumofEvenNumbers(int start,int end)
 	{
-		int res=0;
-		for(int i=a;i<=b;i++)
+		int result=0;
+		for(int i=start;i<=end;i++)
 		{
 			if(i%2==0)
-			res+=i;
+			result+=i;
 		}
-		return res;
+		return result;
 	}
 	
-	public int SumofOdd(int a,int b)
+	public int SumofOddNumbers(int start,int end)
 	{
-		int res=0;
-		for(int i=a;i<=b;i++)
+		int result=0;
+		for(int i=start;i<=end;i++)
 		{
 			if(i%2!=0)
-			res+=i;
+			result+=i;
 		}
-		return res;
-	}
-	
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		SumofNumbers sum= new SumofNumbers();
-		int a,b;
-		System.out.print("Start with ");
-		a=sc.nextInt();
-		System.out.print("\nEnd with ");
-		b=sc.nextInt();
-		System.out.println("\nSum of Even Numbers is "+sum.SumofEven(a, b));
-		System.out.println("\nSum of Odd Numbers is "+sum.SumofOdd(a, b));
+		return result;
 	}
 }
