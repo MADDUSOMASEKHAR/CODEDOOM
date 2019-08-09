c3a0885ff3d9fb13852e812ab1edc2f7bba6366d
MADDUSOMASEKHAR <53407284+MADDUSOMASEKHAR@users.noreply.github.com>
2019-08-09 19:38:45 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,17 +1,10 @@
 package world;
-import java.util.Scanner;
 
 public class helloworld {
 	
-	public static void main(String[] args)
-	{
-		helloworld hello= new helloworld();
-		System.out.println(hello.getMessage());
-	}
 	public String getMessage()
 	{
-		Scanner a=new Scanner(System.in);
-		String message= a.nextLine();
+		String message = "helloworld";
 		return message;
 	}
 
