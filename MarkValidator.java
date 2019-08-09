09c5a9a40e28b8eeafa34ce1385c8ff36c1586e5
MADDUSOMASEKHAR <53407284+MADDUSOMASEKHAR@users.noreply.github.com>
2019-08-09 19:30:52 +0530

    Update MarkValidator.java

--- MarkValidator.java
+++ MarkValidator.java
@@ -1,6 +1,5 @@
-package world;
+package marks;
 
-import java.util.Scanner;
 
 public class MarkValidator {
 	public boolean isPass(int mark)
@@ -22,15 +21,4 @@ public class MarkValidator {
 			return "Grade D";
 	}
 	
-	public static void main(String[] args)
-	{
-		Scanner sc= new Scanner(System.in);
-		MarkValidator mv= new MarkValidator();
-		int a;
-		System.out.println("Enter the marks");
-		a=sc.nextInt();
-		System.out.println(mv.isPass(a));
-		System.out.println(mv.markGrade(a));
-	}
-
 }
