03e0dd7dbfc53af03bb85e5c912002be65a79581
MADDUSOMASEKHAR <53407284+MADDUSOMASEKHAR@users.noreply.github.com>
2019-09-10 22:12:34 +0530

    Update MarkValidatorSwitchCase.java

--- MarkValidatorSwitchCase.java
+++ MarkValidatorSwitchCase.java
@@ -1,35 +1,39 @@
 package markswitchcase;
 
 public class MarkValidatorSwitchCase {
-	
-	public boolean isPass(int mark)
+	if(mark<0)
 	{
-		if(mark>=40)
-			return true;
-		else 
-			return false;
-	}
-	
-	public String markGrade(int mark)
-	{
-		int n=0;
-		if(mark>90)
-			n=1;
-		else if(mark>75)
-			n=2;
-		else if(mark>60)
-			n=3;
-		
-		switch(n)
+		public boolean isPass(int mark)
 		{
-			case 1:
-				return "Grade A";
-			case 2:
-				return "Grade B";
-			case 3:
-				return "Grade C";
-			default:
-				return "Grade D";
+			if(mark>=40)
+				return true;
+			else 
+				return false;
+		}
+	
+		public String markGrade(int mark)
+		{
+			int n=0;
+			if(mark>90)
+				n=1;
+			else if(mark>75)
+				n=2;
+			else if(mark>60)
+				n=3;
+		
+			switch(n)
+			{
+				case 1:
+					return "Grade A";
+				case 2:
+					return "Grade B";
+				case 3:
+					return "Grade C";
+				default:
+					return "Grade D";
+			}
 		}
 	}
+	else 
+		return -1;
 }
