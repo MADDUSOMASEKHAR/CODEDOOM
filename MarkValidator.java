2f0dbc004572a63d3ac21fe1e1bd51d7cb9a76cc
MADDUSOMASEKHAR <53407284+MADDUSOMASEKHAR@users.noreply.github.com>
2019-09-10 22:10:20 +0530

    Update MarkValidator.java

--- MarkValidator.java
+++ MarkValidator.java
@@ -2,23 +2,27 @@ package marks;
 
 
 public class MarkValidator {
-	public boolean isPass(int mark)
+	if(mark<0)
 	{
-		if(mark>=40)
-			return true;
-		else 
-			return false;
+		public boolean isPass(int mark)
+		{
+			if(mark>=40)
+				return true;
+			else 
+				return false;
+		}	
+		public String markGrade(int mark)
+		{
+			if(mark>90)
+				return "Grade A";
+			else if(mark>75)
+				return "Grade B";
+			else if(mark>60)
+				return "Grade C";
+			else
+				return "Grade D";
+		}
 	}
-	public String markGrade(int mark)
-	{
-		if(mark>90)
-			return "Grade A";
-		else if(mark>75)
-			return "Grade B";
-		else if(mark>60)
-			return "Grade C";
-		else
-			return "Grade D";
-	}
-	
+	else 
+		return -1;
 }
