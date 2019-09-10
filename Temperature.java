280720e99c89a66dfdfecaff902def92743000cb
MADDUSOMASEKHAR <53407284+MADDUSOMASEKHAR@users.noreply.github.com>
2019-09-10 22:07:42 +0530

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -3,12 +3,22 @@ package temperature;
 public class Temperature {
 	public double CelsiustoFahrenheit(double celsius)
 	{
-		double result= (celsius*1.8)+32;
-		return result;
+		if(celsius>=0 && celsius<=100)
+		{
+			double result= (celsius*1.8)+32;
+			return result;
+		}
+		else 
+		return -1;
 	}
 	public double FahrenheittoCelsius(double fahrenheit)
 	{
-		double result= ((fahrenheit-32)*5/9);
-		return result;
+		if(fahrenheit>=32 && fahrenheit<=212)
+		{
+			double result= ((fahrenheit-32)*5/9);
+			return result;
+		}
+		else
+		return -1;
 	}
 }
