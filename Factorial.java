68a58c26dcf200c8d30103dd5d90ca87f3fe6d02
MADDUSOMASEKHAR <53407284+MADDUSOMASEKHAR@users.noreply.github.com>
2019-09-10 22:01:25 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -4,10 +4,15 @@ public class Factorial
 {
 	public double calculateFactorial(int number)
 	{
-		double result=1;
-		for(int i=number;i>0;i--)
-			result*=i;
-		return result;
+		if(number>=0)
+		{
+			double result=1;
+			for(int i=number;i>0;i--)
+				result*=i;
+			return result;
+		)
+		else
+		return -1;
 	}
 	
 }
